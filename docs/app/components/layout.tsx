6ff8bc7011c8cfbac6a6202cf44d1a6be05bb94b
Jhon Arias <ariasjhons22@gmail.com>
2026-05-16 17:44:25 +0000

    feat: Squadness Design System Bootstrap v1.0
    - 26 páginas documentadas (Foundations, Componentes, Layouts)
    - Tokens CSS con sistema de 8pt y colores semánticos
    - Componentes Bootstrap 5 con skin Squadness
    - Login de acceso con contraseña compartida
    - Layout List View de aplicación completo
    - Imágenes de ejemplo locales

--- docs/app/components/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { DocsShell } from '@/components/layout/DocsShell'
-
-export default function DocsLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <DocsShell>
-      {children}
-    </DocsShell>
-  )
-}
--- docs/app/getting-started/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { DocsShell } from '@/components/layout/DocsShell'
-
-export default function GettingStartedLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <DocsShell>
-      {children}
-    </DocsShell>
-  )
-}
